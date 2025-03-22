5ca188d3d0473f82d29b1c0017d56a9f41432c5d
Adwaith jayan <aj007boss@gmail.com>
2025-03-22 12:46:14 +0530

    starting

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AuthForm from "@/components/authForm";
+
+export default function Page() {
+      return (
+          <div>
+                <AuthForm  type='sign-up'/>
+          </div>
+      )
+}
