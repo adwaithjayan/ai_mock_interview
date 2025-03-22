f466ada0846fa5c5f8b9149e4120929137919757
Adwaith jayan <aj007boss@gmail.com>
2025-03-22 17:17:16 +0530

    create interview generation ai

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,12 @@ import { getAuth } from "firebase/auth";
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-      apiKey: "AIzaSyCaIg5NsinhricrA_XUP4_ofUw2_3rhszw",
-      authDomain: "mockinterview-8465b.firebaseapp.com",
-      projectId: "mockinterview-8465b",
-      storageBucket: "mockinterview-8465b.firebasestorage.app",
-      messagingSenderId: "34521370030",
-      appId: "1:34521370030:web:0c32ed5771096bd7760c3f",
-      measurementId: "G-RCF6B30XSK"
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+      authDomain:  process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      projectId:  process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      storageBucket:  process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      appId:  process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
